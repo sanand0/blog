fcb3518157648243f2c042d0ad0ac8148a2a3fd3
S Anand <root.node@gmail.com>
2026-08-26 21:22:46 +0800

    Redirect /@... to sanand0.github.io - for goatcounter

--- scripts/test_pagefind.py
+++ scripts/test_pagefind.py
@@ -24,6 +24,8 @@ def test_404_page_loads_search_with_path_query():
     assert "/blog/pagefind/pagefind-ui.js" in html
     assert "search.triggerSearch" in html
     assert "decodeURIComponent(location.pathname" in html
+    assert 'location.pathname.startsWith("/@")' in html
+    assert "https://sanand0.github.io/${location.pathname.slice(2)}${location.search}${location.hash}" in html
     assert "I searched the archive for words in the URL instead." in html
     assert ".pagefind-search .pagefind-ui__results-area { order: -1; }" in html
     assert 'rel="canonical"' not in html
